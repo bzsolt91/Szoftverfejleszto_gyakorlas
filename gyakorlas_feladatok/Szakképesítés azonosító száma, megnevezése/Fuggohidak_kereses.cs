09be2f5583ff706294e391ca1ad814344fef44af
Benei Zsolt <129582837+bzsolt91@users.noreply.github.com>
2025-10-10 18:29:11 +0200

    cr

---
+++ gyakorlas_feladatok/Szakképesítés azonosító száma, megnevezése/Fuggohidak_kereses.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace fuggohidak
+{
+    public partial class kereses : Form
+    {
+        Form frm;
+        List<Fuggohid> hidak;
+        public kereses(Form frm, List<Fuggohid> hidak)
+        {
+            InitializeComponent();
+            this.frm = frm;
+            this.hidak = hidak;
+        }
+
+        private void kereses_Load(object sender, EventArgs e)
+        {
+            betoltes();
+        }
+        private void betoltes()
+        {
+            foreach (var elem in hidak)
+            {
+                if (!cmb_orszag.Items.Contains(elem.Orszag))
+                {
+                    cmb_orszag.Items.Add(elem.Orszag);
+                }
+            }
+        }
+        private List<string> orszagKereses(string orszag, bool km)
+        {
+            List<string> lista = new List<string>();
+            foreach (var elem in hidak)
+            {
+
+                if (elem.Orszag == orszag && km)
+                {
+                    if (elem.Hossz > 1000)
+                        lista.Add(elem.ToString());
+                }
+                else if (elem.Orszag == orszag)
+                    lista.Add(elem.ToString());
+            }
+            return lista;
+        }
+
+        private void btn_kereses_Click(object sender, EventArgs e)
+        {
+            lbx_kereses.Items.Clear();
+            if (cmb_orszag.Text != "")
+                if (cb_1kmnelhosszabbak.Checked)
+                    lbx_kereses.Items.AddRange(orszagKereses(cmb_orszag.Text, true).ToArray());
+                else
+                    lbx_kereses.Items.AddRange(orszagKereses(cmb_orszag.Text, false).ToArray());
+        }
+
+        private void btn_kilepes_Click(object sender, EventArgs e)
+        {
+            frm.Show();
+            this.Close();
+        }
+    }
+}
---
+++ gyakorlas_feladatok/Szakképesítés azonosító száma, megnevezése/fuggohidak_megnyitas.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using fuggohidak;
+namespace fuggohidak
+{
+    public partial class frm_megnyitas : Form
+    {
+        Form fo_form;
+        List<Fuggohid> hidak = new List<Fuggohid>();
+        public frm_megnyitas(Form frm)
+        {
+            InitializeComponent();
+            fo_form = frm;
+        }
+
+        private void frm_megnyitas_Load(object sender, EventArgs e)
+        {
+            beolvasas();
+        }
+        private void beolvasas()
+        {
+            string fejlec = "";
+            try
+            {
+
+                using (var elem = new StreamReader("fuggohidak.csv", Encoding.UTF8))
+                {
+                    string sor = "";
+                    fejlec = elem.ReadLine();
+                    while ((sor = elem.ReadLine()) != null)
+                    {
+                        hidak.Add(new Fuggohid(sor));
+                    }
+                }
+
+
+                foreach (var elemek in hidak)
+                {
+                    lbx_kereses.Items.Add(elemek);
+                }
+            }
+            catch (Exception ex)
+            { MessageBox.Show(ex.Message); }
+
+        }
+
+        private void lbx_kereses_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (lbx_kereses.SelectedIndex != -1)
+            {
+                Fuggohid hid = (Fuggohid)lbx_kereses.SelectedItems[0];
+                tb_hely.Text = hid.Hely;
+                tb_ev.Text = hid.AtadasEve.ToString();
+                tb_hossz.Text = hid.Hossz.ToString();
+                tb_orszag.Text = hid.Orszag;
+
+            }
+        }
+
+        private void rb_2000elott_CheckedChanged(object sender, EventArgs e)
+        {
+            int szamlalo = 0;
+            foreach (var elem in hidak)
+            {
+                if (elem.AtadasEve < 2000)
+                    szamlalo++;
+            }
+            tb_darab.Text = szamlalo.ToString();
+        }
+
+        private void rb_2000utan_CheckedChanged(object sender, EventArgs e)
+        {
+            int szamlalo = 0;
+            foreach (var elem in hidak)
+            {
+                if (elem.AtadasEve >= 2000)
+                    szamlalo++;
+            }
+            tb_darab.Text = szamlalo.ToString();
+        }
+
+        private void smi_kilepes_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void smi_kereses_Click(object sender, EventArgs e)
+        {
+            kereses krs = new kereses(this,hidak);
+            krs.ShowDialog();
+            this.Visible = false;
+        }
+    }
+}
